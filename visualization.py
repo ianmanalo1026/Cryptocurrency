e93dc0498fb9ac5e1ddcfff3b8371c577f1282be
Ian <ian.manalo1026@gmail.com>
2021-02-15 22:57:35 +0800

    DONE WITH DASHBOARD

--- visualization.py
+++ visualization.py
@@ -11,39 +11,39 @@ import plotly
 import plotly.express as px
 import plotly.graph_objs as go
 
+from collections import deque
+
+
+MAX_POINTS_TO_SHOW = 15
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-try:
-    engine = create_engine('postgresql+psycopg2://postgres:soulgun21@127.0.0.1:5432/Cryptocurrency')
-    connection = engine.connect()
-    print("Connected to Successful!")
-except RuntimeError:
-    print('error')
-
-
-
 app.layout = html.Div([
-    dcc.Graph(id='live-graph', animate=True),
+    dcc.Graph(id='live-graph',),
     dcc.Interval(
         id='graph-update',
-        interval=1000,    
-        n_intervals=0
+        interval=1*5000,    
+        n_intervals=0,
+    )]
+    ,title='custom tick labels', 
+    style = {'display': 'inline-block', 'width': '48%'}
     )
-])
 
 @app.callback(Output('live-graph', 'figure'),
               Input('graph-update', 'n_intervals'))
 def update_graph(n):
-    df = pd.read_sql("select * from \"cryptocurrency\"", connection)
-    fig = px.line(df, x="time", y="bitcoin_php")
+    engine = create_engine('postgresql+psycopg2://postgres:soulgun21@127.0.0.1:5432/Cryptocurrency')
+    with engine.connect() as conn:
+        df = pd.read_sql("select * from \"cryptocurrency\"", conn)
+        # fig = go.Figure(data=go.Scatter(x=df["time"], y=df["bitcoin_php"], mode="lines+markers", name="lines+markers"))
+        time = list(deque(df["time"],maxlen=20))
+        php = list(deque(df["bitcoin_php"],maxlen=20))
+        data = go.Scatter(x = time, y=php, name="Scatter", mode="lines+markers")
     
-    
-    return fig
-    
-
+    return {'data': [data],'layout' : go.Layout(xaxis=dict(range=[min(time),max(time)]),
+                                                yaxis=dict(range=[min(php),max(php)]),)}
 
 if __name__ == '__main__':
     app.run_server(debug=True)
